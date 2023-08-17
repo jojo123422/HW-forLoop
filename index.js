8cd513b59533241d8d0a671c2b23a6e422443fc9
jumanah <jumanah.f.14@gmail.com>
2023-08-17 13:56:29 +0300

    add

---
+++ index.js
@@ -0,0 +1,34 @@
+// first exercise
+for (let J =1; J <=10; J++) {
+    if (J==5) {
+        break;
+    }
+    console.log(J);
+}
+// second exercise
+for (let A =3; A <=30; A++){
+    if (A ==10){
+    
+    }
+  console.log(A)
+}
+
+// Third exercise
+let i = 15;
+while (i>5){
+    console.log(i);
+}
+let M = 0;
+do{
+    M++
+    console.log(M)
+} while (M<5)
+
+// *******************
+for (let F=0; F<=5; F++) {
+    let str=("")
+    for(let N =0; N<=N; N++){
+        log (str="*")
+    }
+}
+
